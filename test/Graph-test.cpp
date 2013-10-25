0c82efb87dd98095c1f2a9382a08d2df65e79729
Daniel Haskin <djhaskin987@gmail.com>
2013-10-25 17:57:54 -0600

    Completed graph, need to test

---
+++ test/Graph-test.cpp
@@ -0,0 +1,13 @@
+#include <boost/test/unit_test.hpp>
+#include <cstdlib>
+#include "../src/Sequence.hpp"
+#include "../src/Graph.hpp"
+
+using namespace FireflyAssembler;
+using namespace std;
+
+
+
+BOOST_AUTO_TEST_CASE(Graph_basic_test)
+{
+}
