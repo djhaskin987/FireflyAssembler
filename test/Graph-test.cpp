39bb8bac85eb2239f4dfb10a25b85cb9e0e78a42
Daniel Haskin <djhaskin987@gmail.com>
2013-10-25 18:47:37 -0600

    add graph test fixture

--- test/Graph-test.cpp
+++ test/Graph-test.cpp
@@ -7,7 +7,50 @@ using namespace FireflyAssembler;
 using namespace std;
 
 
+//-- to test --
+//class Graph
+//{
+//    public:
+//        Graph();
+//        Graph(const Graph & other);
+//
+//        Graph & operator = (const Graph & other);
+//
+//        ~Graph();
+//
+//        int sequenceCount() const;
+//        void getSequence(Sequence & seq, int index) const;
+//        bool hasOverlap(int first, int second) const;
+//        bool hasOverlapsFor(int first) const;
+//        int getOverlap(int first, int second) const;
+//        const std::unordered_map<int,int> & getOverlapsFor(int first) const;
+//
+//        void addSequence(const Sequence & sequence);
+//};
+
+class GraphFixture
+{
+    private:
+        vector<Sequence> sequences;
+    public:
+        GraphFixture() : sequences()
+        {
+        }
+        const int size() const
+        {
+            return sequences.size();
+        }
+
+        const Sequence & operator [] (int index)
+        {
+            return sequences[index];
+        }
+}
 
 BOOST_AUTO_TEST_CASE(Graph_basic_test)
 {
+    GraphFixture g = new GraphFixture();
+    Graph graph;
+    
+
 }
