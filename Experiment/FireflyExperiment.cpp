2816e70060f65769b9ca7f1a02e0fbe52d5104d5
Kyle Corbitt <kyle@corbt.com>
2013-11-09 22:21:14 -0700

    framework for experiment

--- Experiment/FireflyExperiment.cpp
+++ Experiment/FireflyExperiment.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <sstream>
+#include <time.h>
 #include <cstdlib>
 #include <cmath>
+#include "FireflyExperiment.h"
+
+using namespace std;
 
 double fitnessFunction(double x, double y)
 {
@@ -16,9 +21,26 @@ double euclideanDistance(double x, double y)
     return sqrt(x*x+y*y);
 }
 
+Firefly::Firefly() {
+	x = (float)rand()/((float)RAND_MAX/10);
+	y = (float)rand()/((float)RAND_MAX/10);
+}
+
+string Firefly::str() {
+	stringstream ss (stringstream::in | stringstream::out);
+	ss << "{" << x << ", " << y << "}";
+	return ss.str();
+}
+
 int main(int argc, char *argv[])
 {
-    // maybe use 5 fireflies for a toy example,
+	srand(time(0));
+
+	Firefly f = Firefly();
+	cout << f.str() << endl;
+
+	Firefly fireflies [10];
+	// maybe use 5 fireflies for a toy example,
     // or a more complicated fitness function and more fireflies,
     // but something to kind of give a "proof-of-concept" for the firefly algorithm
     // before we code it up.
---
+++ Experiment/FireflyExperiment.h
@@ -0,0 +1,23 @@
+/*
+ * FireflyExperiment.h
+ *
+ *  Created on: Nov 9, 2013
+ *      Author: kyle
+ */
+
+#ifndef FIREFLYEXPERIMENT_H_
+#define FIREFLYEXPERIMENT_H_
+
+class Firefly {
+	float x;
+	float y;
+
+public:
+	Firefly();
+	std::string str();
+
+};
+
+
+
+#endif /* FIREFLYEXPERIMENT_H_ */
