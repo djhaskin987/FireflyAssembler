6605fdd06f129a0da38c71b5a751671a1200fa3c
Kyle Corbitt <kyle@corbt.com>
2013-12-02 18:53:25 -0700

    max length argument added and test data

--- resources/sim-reads.py
+++ resources/sim-reads.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 
-# Usage: python sim-reads.py input_file.fa output_file.fa [coverage]
+# Usage: python sim-reads.py input_file.fa output_file.fa [coverage] [max_length]
 
 import sys, random
 from Bio import SeqIO, Seq
 
 DEFAULT_COVERAGE = 5
 AVERAGE_LENGTH = 100
+MAX_LENGTH = 10000
 
-def produce_reads(in_file, out_file, coverage):
-    input_sequence = SeqIO.read(in_file,"fasta").seq.tostring()
-    # print input_sequence.seq.tostring()
+def produce_reads(in_file, out_file, coverage, max_length):
+    input_sequence = SeqIO.read(in_file,"fasta").seq.tostring()[0:max_length]
 
     total_length = coverage*len(input_sequence)
 
@@ -33,14 +33,17 @@ def produce_reads(in_file, out_file, coverage):
 
 
 if len(sys.argv) == 1:
-    produce_reads("./sequences-3.fasta", "./sequences-3.fasta.out", DEFAULT_COVERAGE)
+    produce_reads("./sequences-3.fasta", "./sequences-3.fasta.out", DEFAULT_COVERAGE, MAX_LENGTH)
 
 elif len(sys.argv) == 3:
-    produce_reads(sys.argv[1], sys.argv[2], DEFAULT_COVERAGE)
+    produce_reads(sys.argv[1], sys.argv[2], DEFAULT_COVERAGE, MAX_LENGTH)
 
 elif len(sys.argv) == 4:
-    produce_reads(sys.argv[1], sys.argv[2], int(sys.argv[3]))
+    produce_reads(sys.argv[1], sys.argv[2], int(sys.argv[3]), MAX_LENGTH)
+
+elif len(sys.argv) == 5:
+    produce_reads(sys.argv[1], sys.argv[2], int(sys.argv[3]), int(sys.argv[4]))
+
 
 else:
-    print "Usage: python sim-reads.py input_file.fa output_file.fa [coverage]"
-
+    print "Usage: python sim-reads.py input_file.fa output_file.fa [coverage] [max_length]"
